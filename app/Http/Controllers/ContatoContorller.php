432151cbaf6635015c95a0a8eaed27a56de6b15d
joemust0 <joaofelipe.1607@gmail.com>
2024-02-23 22:44:13 -0300

    Atividade Laravel 02

---
+++ app/Http/Controllers/ContatoContorller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContatoContorller extends Controller
+{
+    //metodos actions
+    public function principal() {
+        echo 'Controller contato';
+    }
+}
---
+++ app/Http/Controllers/SobreNosContorller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SobreNosContorller extends Controller
+{
+    //metodos actions
+        public function principal() {
+            echo 'Controller Sobre Nós';
+        }
+}
